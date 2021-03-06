c98ede5d75daac3519480480cad80e8c7593e37a
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-06 10:56:46 +0530

    called all the methods in main class pushed all the elements removed the last element using pop and traverse all the the element sin the stack and also called the search method to search an  element in the stack

--- src/MyMain/main.java
+++ src/MyMain/main.java
@@ -4,5 +4,16 @@ import Implementation.MyStack;
 
 
 public class main {
-
-}
+    public static void main(String[] args) {
+                int Capacity=5;
+                MyStack stack=new MyStack(Capacity);
+                stack.push(98);
+                stack.push(67);
+                stack.push(81);
+                stack.push(51);
+                stack.traverse();
+                System.out.println("Number of elements in the stack are "+ stack.size());
+                System.out.println("Element removed from the stack is " + stack.pop());
+                System.out.println("searching 20 in stack found "+ stack.search(67));
+            }
+        }
