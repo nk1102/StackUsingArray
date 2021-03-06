3afaf94c09a0922ad2438f5ce8e915c2bf384c24
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-06 10:53:40 +0530

    completed all the implementation of all methods and setted the inital of value of top to 0

--- src/Implementation/MyStack.java
+++ src/Implementation/MyStack.java
@@ -9,13 +9,13 @@ public class MyStack implements StackADT {
     public MyStack(int MAX_CAPACITY) {
         this.MAX_CAPACITY = MAX_CAPACITY;
         arr = new int[MAX_CAPACITY];
-        top=-1;
+        top=0;
     }
 
 
     @Override
     public void push(int element) {
-        if (top != MAX_CAPACITY-1){ // it denotes that stack is  not full
+        if (top != MAX_CAPACITY){ // it denotes that stack is  not full
          arr[top]= element;
          top++;
         }
@@ -41,16 +41,42 @@ public class MyStack implements StackADT {
 
     @Override
     public boolean isempty() {
-        return false;
+
+        return top==0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return top;
     }
 
     @Override
     public int peek() {
-        return 0;
+        int response=0;
+        if (!isempty()){
+            response=arr[top];
+        }
+        else {
+            System.out.println("Stack is empty ");
+        }
+        return response;
+    }
+    public  void traverse(){
+        for (int i = 0; i < top; i++) {
+            System.out.print(arr[i] + ",");
+            
+        }
+        System.out.println();
+    }
+    public boolean search(int searchElement){
+        boolean response=false;
+        for (int i = 0; i < top; i++) {
+            if (arr[i]==searchElement){
+                response=true;
+                break;
+            }
+
+        }
+        return response;
     }
 }
