4de33ffeace3260f6888cf4cd45d2bb514aa3491
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-06 09:18:27 +0530

    created MyStack.java class and implemented all the methods  of StackADT

---
+++ src/Implementation/MyStack.java
@@ -0,0 +1,31 @@
+package Implementation;
+
+import MyInterface.StackADT;
+
+public class MyStack implements StackADT {
+
+    @Override
+    public void push(int element) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public boolean isempty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return 0;
+    }
+}
